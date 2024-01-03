ae6bd3cbc7f5e473ac690b6af2ae3547c204dd36
thulasi-nandan <mulithulasinandan@gmail.com>
2024-01-03 04:40:04 +0000

    blogList

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+//index.js BlockList
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+const BlogList = props => {
+  const {blogsList} = props
+
+  return (
+    <ul className="blog-list">
+      {blogsList.map(eachBlog => (
+        <BlogItem key={eachBlog.id} blogDetails={eachBlog} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
